eb60d33870b0f9044fca3d3d74627d2a31b0c317
caitlinmag <caitlinmaguire14@outlook.com>
2025-11-26 14:18:23 +0000

    Working on Combining the datasets
    Cifar-10 and Cifar-100 have been combined, y train includes the 24 classes however y test currently has 23.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -18,29 +18,33 @@ def main():
         cifar100_classes,
     ) = unpickle()
 
-    # X_train, Y_train, X_test, Y_test = filter_cifar10_by_class(
-    #     train_1_data,
-    #     train_2_data,
-    #     train_3_data,
-    #     train_4_data,
-    #     train_5_data,
-    #     test_data,
-    #     cifar10_classes,
-    # )
+    X_train_1, Y_train_1, X_test_1, Y_test_1 = filter_cifar10_by_class(
+        train_1_data,
+        train_2_data,
+        train_3_data,
+        train_4_data,
+        train_5_data,
+        test_data,
+        cifar10_classes,
+    )
 
-    filter_cifar100_by_class(
+    X_train_2, Y_train_2, X_test_2, Y_test_2 = filter_cifar100_by_class(
         train_dataset_2,
         test_dataset_2,
         cifar100_classes,
     )
 
-    # X_train, Y_train = combine_dataset1_train_data(
-    #     train_1_data,
-    #     train_2_data,
-    #     train_3_data,
-    #     train_4_data,
-    #     train_5_data,
-    # )
+    X_train, Y_train, X_test, Y_test = combine_datasets(
+        X_train_1,
+        Y_train_1,
+        X_test_1,
+        Y_test_1,
+        X_train_2,
+        Y_train_2,
+        X_test_2,
+        Y_test_2,
+    )
+
     # X_train, Y_train, X_test, Y_test = check_data(X_train, Y_train, test_data)
 
 
@@ -141,12 +145,12 @@ def filter_cifar10_by_class(
                 train_labels_list.append(train_labels[i])
 
     # Convert lists to the xtrain, ytrain
-    X_train = np.array(train_data_list)
-    Y_train = np.array(train_labels_list)
+    X_train_1 = np.array(train_data_list)
+    Y_train_1 = np.array(train_labels_list)
 
-    # print("X train shape:", X_train.shape)
-    # print("Y train shape:", Y_train.shape)
-    # print("Y train", np.unique(Y_train))  # checking the labels
+    # print("X train shape:", X_train_1.shape)
+    # print("Y train shape:", Y_train_1.shape)
+    # print("Y train", np.unique(Y_train_1))  # checking the labels
 
     data = test_data[b"data"]
     labels = test_data[b"labels"]
@@ -157,13 +161,13 @@ def filter_cifar10_by_class(
             test_labels_list.append(labels[i])
 
     # Convert lists to the xtrain, ytrain
-    X_test = np.array(test_data_list)
-    Y_test = np.array(test_labels_list)
+    X_test_1 = np.array(test_data_list)
+    Y_test_1 = np.array(test_labels_list)
 
-    # print("X test shape:", X_test.shape)
-    # print("Y test shape:", Y_test.shape)
-    # print("Y test", np.unique(Y_test))  # checking the labels
-    return X_train, Y_train, X_test, Y_test
+    # print("X test shape:", X_test_1.shape)
+    # print("Y test shape:", Y_test_1.shape)
+    # print("Y test", np.unique(Y_test_1))  # checking the labels
+    return X_train_1, Y_train_1, X_test_1, Y_test_1
 
 
 def filter_cifar100_by_class(
@@ -194,20 +198,35 @@ def filter_cifar100_by_class(
     # ]
 
     # cattle, fox, baby, boy, girl, man, woman, rabbit, squirrel, bicycle, bus, motorcycle, pickup_truck, train, lawn_mower, tractor
-    required_classes_num = [2, 8, 11, 13, 19, 34, 35, 41, 46, 48, 58, 65, 80, 89, 90, 98]
+    required_classes_num = [
+        2,
+        8,
+        11,
+        13,
+        19,
+        34,
+        35,
+        41,
+        46,
+        48,
+        58,
+        65,
+        80,
+        89,
+        90,
+        98,
+    ]
     # trees
     required_superclasses_num = [17]
-
 
     train_data_list = []
     test_data_list = []
 
-    train_fine_labels_list = []
     test_fine_labels_list = []
 
-    train_coarse_labels_list = []
     test_coarse_labels_list = []
 
+    train_labels_list = []
 
     train_data = train_dataset_2[b"data"]
     train_fine_labels = train_dataset_2[b"fine_labels"]
@@ -217,30 +236,27 @@ def filter_cifar100_by_class(
     test_fine_labels = test_dataset_2[b"fine_labels"]
     test_coarse_labels = test_dataset_2[b"coarse_labels"]
 
-
     for i in range(len(train_fine_labels)):
         if train_fine_labels[i] in required_classes_num:
             train_data_list.append(train_data[i])
-            train_fine_labels_list.append(train_fine_labels[i])
-
+            train_labels_list.append(train_fine_labels[i])
 
     for i in range(len(train_coarse_labels)):
         if train_coarse_labels[i] in required_superclasses_num:
             train_data_list.append(train_data[i])
-            train_coarse_labels_list.append(train_coarse_labels[i])
+            train_labels_list.append(train_coarse_labels[i])
 
     # Combine Class and Superclass Label Lists
-    train_labels_list = train_fine_labels_list + train_fine_labels_list
+    # train_labels_list = train_fine_labels_list + train_fine_labels_list
 
     # Convert Lists to X_train, Y_train
-    X_train = np.array(train_data_list)
-    Y_train = np.array(train_labels_list)
+    X_train_2 = np.array(train_data_list)
+    Y_train_2 = np.array(train_labels_list)
 
     for i in range(len(test_fine_labels)):
         if test_fine_labels[i] in required_classes_num:
             test_data_list.append(test_data[i])
             test_fine_labels_list.append(test_fine_labels[i])
-
 
     for i in range(len(test_coarse_labels)):
         if test_coarse_labels[i] in required_superclasses_num:
@@ -250,47 +266,79 @@ def filter_cifar100_by_class(
     # Combine Class and Superclass Label Lists
     test_labels_list = test_fine_labels_list + test_coarse_labels_list
     # Convert Lists to X_test, Y_test
-    X_test = np.array(test_data_list)
-    Y_test = np.array(test_labels_list)
+    X_test_2 = np.array(test_data_list)
+    Y_test_2 = np.array(test_labels_list)
 
-    # print("X test shape:", X_test.shape)
-    # print("Y test shape:", Y_test.shape)
-    # print("Y test", np.unique(Y_test))
+    # print("X train shape:", X_train_2.shape)
+    # print("Y train shape:", Y_train_2.shape)
+    # print("Y train", np.unique(Y_train_2))
+    # print("X test shape:", X_test_2.shape)
+    # print("Y test shape:", Y_test_2.shape)
+    # print("Y test", np.unique(Y_test_2))
+    return X_train_2, Y_train_2, X_test_2, Y_test_2
+
+
+def combine_datasets(
+    X_train_1, Y_train_1, X_test_1, Y_test_1, X_train_2, Y_train_2, X_test_2, Y_test_2
+):
+    cifar_10_labels = [
+        b"automobile",
+        b"bird",
+        b"cat",
+        b"deer",
+        b"dog",
+        b"horse",
+        b"truck",
+    ]
+    cifar_100_labels = [
+        b"cattle",
+        b"fox",
+        b"baby",
+        b"boy",
+        b"girl",
+        b"man",
+        b"woman",
+        b"rabbit",
+        b"squirrel",
+        b"bicycle",
+        b"bus",
+        b"motorcycle",
+        b"pickup_truck",
+        b"train",
+        b"lawn_mower",
+        b"tractor",
+        b"trees",
+    ]
+
+    # mapping unique ids to each class name
+    all_labels = sorted(list(set(cifar_10_labels) | set(cifar_100_labels)))
+    unique_id = {name: i for i, name in enumerate(all_labels)}
+
+    cifar_10_new = np.array([unique_id[name] for name in cifar_10_labels])
+    cifar_100_new = np.array([unique_id[name] for name in cifar_100_labels])
+
+    X_train = np.vstack([X_train_1, X_train_2])
+    Y_train = np.hstack([cifar_10_new, cifar_100_new])
+
+    X_test = np.vstack([X_test_1, X_test_2])
+    Y_test = np.hstack([Y_test_1, Y_test_2])
+
+    print("X train:", X_train.shape)
+    print("Y train:", Y_train.shape)
+    print("X test:", X_test.shape)
+    print("Y test:", Y_test.shape)
+    print("Y train", np.unique(Y_train))
+    print("Y test", np.unique(Y_test))
+
+    train_count = len(np.unique(Y_train))
+    print("Y train count:", train_count)
+
+    test_count = len(np.unique(Y_test))
+    print("Y test count", test_count)
     return X_train, Y_train, X_test, Y_test
 
 
-# we could potentially cut out the need for this function, and just defin
-def combine_dataset1_train_data(
-    train_1_data, train_2_data, train_3_data, train_4_data, train_5_data
-):
-
-    X_train = np.vstack(
-        [
-            train_1_data[b"data"],
-            train_2_data[b"data"],
-            train_3_data[b"data"],
-            train_4_data[b"data"],
-            train_5_data[b"data"],
-        ]
-    )
-
-    Y_train = np.hstack(
-        [
-            train_1_data[b"labels"],
-            train_2_data[b"labels"],
-            train_3_data[b"labels"],
-            train_4_data[b"labels"],
-            train_5_data[b"labels"],
-        ]
-    )
-    print(X_train.shape)
-    print(Y_train.shape)
-    return X_train, Y_train
-
-
 def check_data(X_train, Y_train, test_data):
-    # we have already defined the features and labels for x_train and y_train
-
     # print("Show type train: ", type(train_data))
     print("Show type test:", type(test_data))
 
